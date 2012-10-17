772b2ca15a5b512db658ddedee11a1d0533a1b55
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-17 14:05:26 -0600

    Add startup code of http server

--- Mirror/ASEAppDelegate.m
+++ Mirror/ASEAppDelegate.m
@@ -1,10 +1,38 @@
 #import "ASEAppDelegate.h"
 
+#import <RoutingHTTPServer/RoutingHTTPServer.h>
+#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/DDTTYLogger.h>
+
+const static int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@interface ASEAppDelegate ()
+
+@property (strong) RoutingHTTPServer * httpServer;
+
+@end
+
 @implementation ASEAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    DDLogVerbose(@"%@: %@", THIS_FILE, THIS_METHOD);
+    
+    self.httpServer = [RoutingHTTPServer new];
+    self.httpServer.type = @"_airplay._tcp.";
+    self.httpServer.port = 7000;
+    
+    NSError * error;
+    
+    if(![self.httpServer start:&error]) {
+        DDLogError(@"%@: Could not start the HTTP server: %@", THIS_FILE, error);
+    }
+}
+
+-(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
 }
 
 @end
